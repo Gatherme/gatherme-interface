36fe457769f9e5128cf8668be1a1173a17a4c95c
ssalamncag <jssalamancag@unal.edu.co>
2020-05-31 12:34:35 -0500

    add axios request

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const fs = require('fs');
 const soap = require(`soap`);
+const axios = require('axios')
 
 
 const soapServer = require('./soap-server/index')
@@ -10,8 +11,14 @@ const ss_2a = require('./soap-server/service')
 const xml = fs.readFileSync('./soap-server/ss_2a_service.wsdl', 'utf8');
 //create an express server and pass it to a soap server
 
+async function data (){
+    let res = await axios.get("http://172.17.0.1:" + "3000/gatherme-users-ms" + "/user-username/" + "jssalamancag")
+    console.log(res.data[0]['likes']);
+}
+
 soapServer.listen(3030, function(){
     soap.listen(soapServer, '/ss_2a', ss_2a, xml, function(){
+        data();
         console.log("servidor corriendo")
     })
 })
--- soap-server/operations.js
+++ soap-server/operations.js
@@ -1,7 +1,13 @@
 function getLikes(args){
-    let idUser = args
-    console.log(idUser)
-    let  likes = ["uno", "dos"]
+    let idUser = args;
+    let  likes = [];
+    console.log(idUser);
+    async function data (idUser){
+        let res = await axios.get("http://172.17.0.1:" + "3000/gatherme-users-ms" + "/user-username/" + idUser)
+        console.log(res.data[0]['likes']);
+        likes = res.data[0]['likes']
+    }
+    
     return {likes: likes}
 
 }
