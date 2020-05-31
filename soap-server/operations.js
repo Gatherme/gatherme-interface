1062ef9a20e9450141e09189651f0cd00ad89a89
ssalamncag <jssalamancag@unal.edu.co>
2020-05-31 12:59:39 -0500

    fix url request

--- soap-server/operations.js
+++ soap-server/operations.js
@@ -3,8 +3,9 @@ async function getLikes(args) {
     let idUser = args;
     let likes = [];
     console.log(idUser);
+    let host = '172.17.0.1'
 
-    let res = await axios.get("http://172.17.0.1:" + "3000/gatherme-users-ms" + "/user-username/" + idUser)
+    let res = await axios.get(`http://${host}:3000/gatherme-users-ms/user-username/${idUser}`)
     console.log(res.data[0]['likes']);
     likes = res.data[0]['likes']
 
